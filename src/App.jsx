8f7462bfa14ddbd7cecfb0d7c4d276b4634b2033
Tarun <tarunkumartadala@gmail.com>
2024-12-12 10:01:21 +0530

    form content is created

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import "./App.css"
 function App() {
   return (
     <div className='main-container'>
-    <h1>Hello</h1>
+    <h1>Gen AI Web App By Tarun</h1>
+   
+    <div className="form-container">
+
+      <div>
+        <label> Enter Query</label>
+        <input type="text" placeholder="Enter " className="query-input"/>
+      </div>
+
+   </div>
     </div>
   )
 }
